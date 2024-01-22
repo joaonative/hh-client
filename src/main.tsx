c7a30b64299399e99a54daeee939ff2fb4eaf958
joaonative <jestudos127@gmail.com>
2024-01-22 17:01:31 -0300

    feat: googleauth + authprovider

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AuthProvider } from "./AtuhContext.tsx";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId="324184292512-fav2u8522eehge9fih76q0j5qark0qha.apps.googleusercontent.com">
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
