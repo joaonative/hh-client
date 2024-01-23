eb5b1871fe8697cd6aa15b357d47d295161f517c
joaonative <jestudos127@gmail.com>
2024-01-23 15:15:56 -0300

    more svgs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,34 +1,5 @@
-import { useGoogleLogin } from "@react-oauth/google";
-import { useAuth } from "../AtuhContext";
-
 function Home() {
-  const { isLoggedIn, login, logout } = useAuth();
-
-  const loginGoogle = useGoogleLogin({
-    onSuccess: async (response) => {
-      //passamos o token para o contexto se comunicar com o google de forma permitida como a resposta da biblioteca @react-oauth/google que definimos em main.tsx juntamente com nosso clientId do google
-      login(response.access_token);
-    },
-  });
-
-  return (
-    <>
-      <button onClick={() => loginGoogle()} className="p-2 bg-green-500">
-        Google login
-      </button>
-
-      {isLoggedIn ? (
-        <>
-          <h1>Logado</h1>
-          <button onClick={() => logout()} className="p-2 bg-green-500">
-            Sair
-          </button>
-        </>
-      ) : (
-        <h2>Deslogado</h2>
-      )}
-    </>
-  );
+  return <>home page</>;
 }
 
 export default Home;
