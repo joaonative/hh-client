f7fcc5aa2af1c2d3e659dd5b2a6031fc9a2cbd69
joaonative <jestudos127@gmail.com>
2024-01-22 23:09:16 -0300

    feat: new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#8D6CCF",
+        background: "#1E1E1E",
+        darker: "#101010",
+      },
+    },
   },
   plugins: [],
 };
