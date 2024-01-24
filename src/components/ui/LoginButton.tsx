ce96d3bc0228dd8320b0a3280449798ea00ed6ec
joaonative <jestudos127@gmail.com>
2024-01-24 11:36:02 -0300

    feat: skipping login fn if the user is logged in

--- src/components/ui/LoginButton.tsx
+++ src/components/ui/LoginButton.tsx
@@ -1,6 +1,7 @@
 import { useGoogleLogin } from "@react-oauth/google";
 import { useAuth } from "../../AtuhContext";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Dialog from "./Dialog";
 
 interface LoginBtnProps {
@@ -12,22 +13,28 @@ interface LoginBtnProps {
 function LoginButton({ name, icon, isOpen }: LoginBtnProps) {
   const { isLoggedIn, login, logout, userData } = useAuth();
 
+  const navigate = useNavigate();
+
   const [isDialogOpen, setIsDialogOpen] = useState(false);
 
-  const formattedName = userData?.given_name
-    ? userData.given_name.charAt(0).toUpperCase() +
-      userData.given_name.slice(1).toLowerCase()
+  const formattedName = userData?.name
+    ? userData.name.charAt(0).toUpperCase() +
+      userData.name.slice(1).toLowerCase()
     : "";
 
   const loginGoogle = useGoogleLogin({
     onSuccess: async (response) => {
       //passamos o token para o contexto se comunicar com o google de forma permitida como a resposta da biblioteca @react-oauth/google que definimos em main.tsx juntamente com nosso clientId do google
-      login(response.access_token);
+      login(response.access_token, () => {
+        navigate("/home");
+      });
     },
   });
 
   const handleConfirm = () => {
-    logout();
+    logout(() => {
+      navigate("/");
+    });
     setIsDialogOpen(false);
   };
 
@@ -46,10 +53,12 @@ function LoginButton({ name, icon, isOpen }: LoginBtnProps) {
             <img
               src={userData?.picture}
               alt={`User picture`}
-              className="md:w-8 md:h-8 w-6 rounded-full transition-all duration-300"
+              className="lg:w-8 lg:h-8 w-10 rounded-full transition-all duration-300"
             />
             {isOpen && (
-              <h1 className={`md:w-36 w-28 text-base text-start capitalize`}>
+              <h1
+                className={`lg:w-36 w-40 text-base text-start capitalize overflow-ellipsis overflow-hidden whitespace-nowrap`}
+              >
                 {formattedName}
               </h1>
             )}
@@ -60,9 +69,9 @@ function LoginButton({ name, icon, isOpen }: LoginBtnProps) {
           onClick={() => loginGoogle()}
           className="flex gap-3 items-center justify-center px-4 py-2 rounded-xl hover:bg-background border border-darker transition-all duration-300"
         >
-          <img src={icon} alt={`${name} icon`} className="w-10 md:w-8" />
+          <img src={icon} alt={`${name} icon`} className="w-10 lg:w-8" />
           {isOpen && (
-            <h1 className={`md:w-36 w-40 text-base text-start`}>{name}</h1>
+            <h1 className={`lg:w-36 w-40 text-base text-start`}>{name}</h1>
           )}
         </button>
       )}
