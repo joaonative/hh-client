f8fc3302521679088cd2ad953053ff8ee93bf718
joaonative <jestudos127@gmail.com>
2024-01-24 11:32:50 -0300

    rm: authprovider from root

--- src/main.tsx
+++ src/main.tsx
@@ -8,9 +8,7 @@ import { AuthProvider } from "./AtuhContext.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="324184292512-fav2u8522eehge9fih76q0j5qark0qha.apps.googleusercontent.com">
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
