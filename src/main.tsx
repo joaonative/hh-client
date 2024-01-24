a60a6db0e1c998f1bd3afae9447e5835f1a24fa4
joaonative <jestudos127@gmail.com>
2024-01-24 11:43:49 -0300

    fix: secret moved to env

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { AuthProvider } from "./AtuhContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="324184292512-fav2u8522eehge9fih76q0j5qark0qha.apps.googleusercontent.com">
+    <GoogleOAuthProvider
+      clientId={import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID}
+    >
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
