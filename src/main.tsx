a4bd6301d2371f2389b16951b2e73c61d226bdcf
joaonative <jestudos127@gmail.com>
2024-01-25 16:12:31 -0300

    new dep react-query

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "./lib/react-query.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider
-      clientId={import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID}
-    >
-      <App />
-    </GoogleOAuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <GoogleOAuthProvider
+        clientId={import.meta.env.VITE_REACT_APP_GOOGLE_CLIENT_ID}
+      >
+        <App />
+      </GoogleOAuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
