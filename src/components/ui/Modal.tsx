3de79134abc6835b38a346887d874462ddae5e6f
joaonative <jestudos127@gmail.com>
2024-01-26 16:28:29 -0300

    feat: new modal component

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,14 +1,22 @@
 import { motion } from "framer-motion";
 import Button from "./Button";
+import { ReactNode } from "react";
 
 interface ModalProps {
   message: string;
   confirmMessage?: string;
-  onConfirm?: (params?: Record<string, any>) => Promise<void>;
+  onConfirm?: () => void;
   onCancel?: () => void;
+  children: ReactNode;
 }
 
-function Modal({ message, confirmMessage, onConfirm, onCancel }: ModalProps) {
+function Modal({
+  message,
+  confirmMessage,
+  onConfirm,
+  onCancel,
+  children,
+}: ModalProps) {
   const handleConfirm = () => {
     if (onConfirm) {
       onConfirm();
@@ -28,12 +36,18 @@ function Modal({ message, confirmMessage, onConfirm, onCancel }: ModalProps) {
       transition={{ duration: 0.5 }}
       className="fixed inset-0 flex items-center justify-center z-50"
     >
-      <div className="flex flex-col items-center justify-center gap-5 bg-darker border border-background px-10 py-5 rounded-xl">
-        <h1 className="text-xl">{message}</h1>
-        <div className="flex items-center justify-center gap-5">
-          <Button name={confirmMessage} handleClick={handleConfirm} secondary />
-          <Button name="X" handleClick={handleCancel} danger />
+      <div className="flex flex-col items-center justify-center gap-5 bg-darker border border-background w-max p-6 rounded-lg">
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="text-2xl text-primary">{message}</h1>
+          <Button handleClick={handleCancel} close />
         </div>
+        {children}
+        <Button
+          name={confirmMessage}
+          handleClick={handleConfirm}
+          primary
+          full
+        />
       </div>
     </motion.div>
   );
