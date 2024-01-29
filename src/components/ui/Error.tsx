5c253b8979d90a22fda5cce31da838c38af29759
joaonative <jestudos127@gmail.com>
2024-01-29 17:36:34 -0300

    fix: optmizing

--- src/components/ui/Error.tsx
+++ src/components/ui/Error.tsx
@@ -1,11 +1,21 @@
+import Button from "./Button";
+
 interface ErrorPopUpProps {
   message: string;
 }
 
 function ErrorPopUp({ message }: ErrorPopUpProps) {
   return (
-    <div className="opacity-50 bg-darker border border-red-500 px-5 py-2 w-full text-center">
+    <div className="opacity-50 bg-darker border border-red-500 px-5 py-2 w-full text-center flex flex-col gap-2">
       {message}
+      <Button
+        handleClick={() => {
+          window.location.reload();
+        }}
+        full
+        danger
+        name="Reload page"
+      />
     </div>
   );
 }
