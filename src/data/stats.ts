e3661d2a36c56c5f9da466d7878a1d04bf2e0ab8
joaonative <jestudos127@gmail.com>
2024-01-30 11:18:58 -0300

    feat: stats fetching

---
+++ src/data/stats.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosError } from "axios";
+
+const apiUrl = "http://localhost:8080/api";
+
+const token = localStorage.getItem("token-acesso");
+
+export interface Stats {
+  totalMonthlyOccurrences: number;
+  totalGoodOccurrences: number;
+  totalBadOccurrences: number;
+  percentChangeGood: number | null;
+  percentChangeBad: number | null;
+  percentChangeTotal: number | null;
+}
+
+export async function getUserStats(userId: string | undefined) {
+  try {
+    const res = await axios.get(`${apiUrl}/users/${userId}`, {
+      headers: {
+        Authorization: token,
+      },
+    });
+    console.log(res.data);
+    return res.data;
+  } catch (err) {
+    return err;
+  }
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,35 @@
+import { useQuery } from "react-query";
+import { useAuth } from "../AtuhContext";
 import Container from "../components/layout/Container";
+import GridContainer from "../components/layout/GridContainer";
 import SideBar from "../components/layout/SideBar";
+import Greeting from "../components/ui/Greeting";
+import HourHabitBox from "../components/ui/HourHabitBox";
+import { Stats, getUserStats } from "../data/stats";
 
 function Home() {
+  const { userData } = useAuth();
+
+  const { data: stats } = useQuery<Stats>({
+    queryKey: ["stats"],
+    queryFn: async () => getUserStats(userData?.email),
+  });
   return (
     <>
       <SideBar />
       <Container>
-        <h1>teste</h1>
+        <div className="flex flex-col gap-3">
+          <Greeting />
+          <GridContainer>
+            {stats?.totalBadOccurrences && (
+              <HourHabitBox
+                percentage={stats.percentChangeBad}
+                quantity={stats.totalBadOccurrences}
+                bad
+              />
+            )}
+          </GridContainer>
+        </div>
       </Container>
     </>
   );
